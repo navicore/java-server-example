f58c80add2781653ba7ebeafca09cd48a25eda52
Ed Sweeney <ed@onextent.com>
2022-10-12 19:04:51 -0700

    clil

--- src/main/java/navicore/demo/Server.java
+++ src/main/java/navicore/demo/Server.java
@@ -1,13 +1,65 @@
 package navicore.demo;
 
-/**
- * Hello world!
- *
- */
-public class Server 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+@Command(name = "Server", version = "Server 1.0", mixinStandardHelpOptions = true)
+public class Server implements Runnable {
+
+    @CommandLine.Option(names = {"-k", "--keystore"},
+               description = "Full path to the keystore (jks file).")
+    String keystorePath = null;
+
+    @CommandLine.Option(names = {"-w", "--webhook"},
+               description = "URL to GET when processing webhook.")
+    String webhookUrl = null;
+
+    @Override
+    public void run() {
+        if (keystorePath != null) {
+            System.out.println("Keystore: " + keystorePath);
+        }
+        if (webhookUrl != null) {
+            System.out.println("Webhook: " + webhookUrl);
+        }
+        HttpServer server;
+        try {
+            server = HttpServer.create(
+                new InetSocketAddress("localhost", 8001), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        server.createContext("/test", new MyHttpHandler());
+        server.start();
+    }
+
+    private static class MyHttpHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            OutputStream outputStream = httpExchange.getResponseBody();
+            String msg = "Sever did something.\n";
+            httpExchange.sendResponseHeaders(200, msg.length());
+            outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+            outputStream.close();
+            System.out.println("sent: " + msg);
+        }
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Server()).execute(args);
+        if (exitCode > 0) {
+            System.exit(exitCode);
+        }
     }
 }
